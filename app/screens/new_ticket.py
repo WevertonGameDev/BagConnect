88bc627404d907f7f7f78edb79a149004ea8a261
WevertonGameDev <weverton.gamedev@gmail.com>
2026-07-21 00:28:22 -0300

    Atualizado Idioma new_ticket

--- app/screens/new_ticket.py
+++ app/screens/new_ticket.py
@@ -1,11 +1,61 @@
+from kivy.app import App
+from kivy.properties import StringProperty
+
 from kivymd.uix.screen import MDScreen
 from kivymd.toast import toast
 
 
 class NewTicketScreen(MDScreen):
 
+
+    titulo = StringProperty("")
+    descricao_ticket = StringProperty("")
+    assunto = StringProperty("")
+    descricao_label = StringProperty("")
+    cpf = StringProperty("")
+    codigo_bagagem = StringProperty("")
+    criar = StringProperty("")
+    campo_obrigatorio = StringProperty("")
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        app = App.get_running_app()
+
+        if app:
+
+            app.bind(
+                idioma_evento=self._idioma_alterado
+            )
+
+
+    def on_pre_enter(self):
+        self.atualizar_textos()
+
+
+    def _idioma_alterado(self, *args):
+        self.atualizar_textos()
+
+
+
+    def atualizar_textos(self):
+        app = App.get_running_app()
+
+
+        self.titulo = app.tr("novo_ticket")
+        self.descricao_ticket = app.tr("ticket_descricao")
+        self.assunto = app.tr("ticket_assunto")
+        self.descricao_label = app.tr("ticket_descricao_label")
+        self.cpf = app.tr("ticket_cpf")
+        self.codigo_bagagem = app.tr("ticket_codigo")
+        self.criar = app.tr("ticket_criar")
+        self.campo_obrigatorio = app.tr("campo_obrigatorio")
+
+
     def voltar(self):
         self.manager.current = "support"
+
 
     def criar_ticket(self):
 
@@ -36,10 +86,7 @@ class NewTicketScreen(MDScreen):
                 valido = False
 
         if not valido:
-
-            toast(
-                "Preencha todos os campos."
-            )
+            toast(App.get_running_app().tr("campo_obrigatorio"))
 
             return
 
